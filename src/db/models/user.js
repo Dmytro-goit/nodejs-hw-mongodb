5cfa2fc03fb5edc2fb0fe39918b94ad2b9802b52
Dmytro-goit <dmytro.moiyseev@gmail.com>
2025-08-29 14:19:33 +0300

    Update user.js

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -24,16 +24,3 @@ usersSchema.methods.toJSON = function () {
 };
 
 export const UsersCollection = model('users', usersSchema);
-
-const sessionsSchema = new Schema(
-  {
-    userId: { type: String, required: true },
-    accessToken: { type: String, required: true },
-    refreshToken: { type: String, required: true },
-    accessTokenValidUntil: { type: Date, required: true },
-    refreshTokenValidUntil: { type: Date, required: true },
-  },
-  { timestamps: true, versionKey: false },
-);
-
-export const SessionsCollection = model('sessions', sessionsSchema);
